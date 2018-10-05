28cc0e482aa6bb9835b2383135296e60506c7582
Marcus Hobdy <hobdy.marcusjr@gmail.com>
2018-10-04 21:37:05 -0400

    updated servciceWorker.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker();
